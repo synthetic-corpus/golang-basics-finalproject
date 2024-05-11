466b545d39bb5b907af64da111e694ed75041a55
Joel G <joel@joelgonzaga.com>
2024-05-11 12:53:51 -0700

    Fake Database and fake users are set up.

--- project/go.mod
+++ project/go.mod
@@ -3,3 +3,5 @@ module udacity-crm
 go 1.22.2
 
 require github.com/gorilla/mux v1.8.1
+
+require github.com/google/uuid v1.6.0 // indirect
--- project/main.go
+++ project/main.go
@@ -5,10 +5,53 @@ import (
 	"fmt"
 	"net/http"
 	"os"
-
+	"github.com/google/uuid"
 	"github.com/gorilla/mux"
+	//"net/mail"
 )
 
+// Structs section
+type Customer struct {
+	ID string // always a google uuid
+	Name string
+	Role string
+	Email string
+	Phone string
+	Contacted bool
+}
+
+// The fake Database
+type Database struct {
+	Customers map[string]Customer
+}
+
+// The Database Level CRRUD methods
+
+func (db *Database) writeCustomer(newCustomer Customer) bool {
+	db.Customers[newCustomer.ID] = newCustomer
+	return true // will put this into a try catch block later
+}
+
+func (db *Database) retrieveAll() map[string]Customer {
+	return db.Customers
+}
+
+func (db *Database) retrieveOne(ID string) Customer {
+	return db.Customers[ID] // will add better error handling later
+}
+
+func(db *Database) updateOne(ID string, customer Customer){
+	// Logic used here should prevent users from using this to alter the UUID.
+	customer.ID = ID
+	db.Customers[ID] = customer
+}
+
+func(db *Database) deleteOne(ID string){
+	delete(db.Customers, ID)
+}
+
+
+// The API fucntions
 func helloAPI(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "application/json")
 
@@ -88,6 +131,44 @@ func deleteUser(w http.ResponseWriter, r *http.Request) {
 }
 
 func main() {
+	// Instatiates an empty Database
+	var myFakeDatabase Database = Database{
+		Customers: map[string]Customer{},
+	}
+
+// Populates the Database
+	var myFakeUsers []Customer = []Customer{
+		{	
+			ID: uuid.NewString(),
+			Name: "John Doe",
+			Role: "Buyer",
+			Email: "yourEmail@google.net",
+			Phone: "818-555-1515",
+			Contacted: false,
+		},
+		{
+			ID: uuid.NewString(),
+			Name: "Allison Looper",
+			Role: "Test Merchant",
+			Email: "allyloop1990@gmail.com",
+			Phone: "626-555-8055",
+			Contacted: false,
+		},
+		{
+			ID: uuid.NewString(),
+			Name: "Holly Friedman",
+			Role: "Another person",
+			Email: "Hollyofthevalley@protonmail.com",
+			Phone: "818-555-7112",
+			Contacted: false,
+		},
+	}
+	// Populate database
+	for _,person := range myFakeUsers{
+		myFakeDatabase.writeCustomer(person)
+	}
+	fmt.Println(myFakeDatabase.Customers)
+
 	router := mux.NewRouter()
 	router.HandleFunc("/hello", helloAPI).Methods("GET")
 	router.HandleFunc("/users", createUser).Methods("POST")
